7bd95162af641c04360ae02fc56a971f96121756
Sergey Guselnikov <guselnikov@me.com>
2016-02-22 17:58:46 +0300

    Nesting solution

---
+++ 7.3.swift
@@ -0,0 +1,32 @@
+public func solution(inout S : String) -> Int {
+    // write your code in Swift 2.2
+    if S.characters.count == 0 {
+        return 1
+    }
+
+    if S.characters.count % 2 > 0 {
+        return 0
+    }
+
+    var a = S.characters.map {String($0)}
+
+    var b: [String] = []
+
+    for i in 0..<a.count {
+        if a[i] == "(" {
+            b.append(a[i])
+        } else {
+            if b.last == "(" {
+                b.removeLast()
+            } else {
+                return 0
+            }
+        }
+    }
+
+    if b.count > 0 {
+        return 0
+    } else {
+        return 1
+    }
+}
